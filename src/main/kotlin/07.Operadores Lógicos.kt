7479eb96eb8dba716e6f366d6ce4d1f5507b5fa4
Gabriel <gabrieljknd@gmail.com>
2023-02-21 23:45:51 -0300

    Operadores Logicos

---
+++ src/main/kotlin/07.Operadores Lógicos.kt
@@ -0,0 +1,38 @@
+fun main() {
+    //true && true && true
+    if (20 == 20 && 10 < 20 && 50>30) {
+        println("Verdadeiro")
+    } else {
+        println("Falso")
+    }
+}
+/*      true && false
+    if (20 == 20 && 20 < 20)
+        println("Verdadeiro")
+    } else {
+        println("Falso")
+    }
+
+           false && false
+     if (20 == 10 && 20 < 20)
+        println("Verdadeiro")
+    } else {
+        println("Falso")
+    }
+
+    false && false = false        false || false = false
+    true && true = true           false || true = true
+    false && true = false         true || true = true
+    true && false = false         true || false = true
+
+
+    if (20 == 20 || 20 <20)
+        println("Verdadeiro")
+    } else {
+        println("Falso")
+    }
+
+ */
+//Operadores lógicos
+//&& E
+//|| Ou
