f90c9159d45c43b4d3ae8bab7b708f5d65b079f1
Gabriel <gabrieljknd@gmail.com>
2023-02-22 00:16:13 -0300

    For

--- src/main/kotlin/09.Estrutura de Repetição While.kt
+++ src/main/kotlin/09.Estrutura de Repetição While.kt
@@ -1,5 +1,5 @@
 fun main (){
-    //Estrutura de repetição While (enquanto)
+    //Estrutura de repetição For (para)
 
                         // Indice  //0     //1      //2     //3     //4     //5    //6       //7    //8     //9
     var listadecompras = arrayOf("Arroz","Feijão","Açucar","Leite","Carne","Uva","Batata","Banana","Ovos","Frango",)
@@ -7,13 +7,12 @@ fun main (){
     var indice = 0
     println(listadecompras.size)
 
-        //Enquanto a condição do while for verdadeiro ira ficar em loop se for false terminano loop.
-    while (indice <= listadecompras.size -1 ){
+              //in = intervalo
+    for (indice in 0..9){  // 0 until 10
 
         println(listadecompras[indice])
-        indice++
 
+    }
 
-     }
 }
 
