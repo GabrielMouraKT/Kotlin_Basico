51f32cbfc6c92dfa94a7d086c4a9293ba43a72e7
Gabriel <gabrieljknd@gmail.com>
2023-02-22 00:32:53 -0300

    For

--- src/main/kotlin/10.Estrutura de Repetição For.kt
+++ src/main/kotlin/10.Estrutura de Repetição For.kt
@@ -1,14 +1,18 @@
 fun main (){
     //Estrutura de repetição For (para)
 
-    // Indice  //0     //1      //2     //3     //4     //5    //6       //7    //8     //9
+                        // Indice  //0     //1      //2     //3     //4     //5    //6       //7    //8     //9
     var listadecompras = arrayOf("Arroz","Feijão","Açucar","Leite","Carne","Uva","Batata","Banana","Ovos","Frango",)
 
     var indice = 0
+    println(listadecompras.size)
 
+              //in = intervalo
+    for (indice in 0..9){  // 0 until 10
 
-    while (indice <=9){
         println(listadecompras[indice])
-        indice ++
+
     }
+
 }
+
